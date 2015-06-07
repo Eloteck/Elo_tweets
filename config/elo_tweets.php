0670bf12f54f53545638eb7655aad29f853e6769
Eloteck <eloteck@outlook.fr>
2015-06-07 22:19:49 +0200

    Add elo_tweets in GitHub

---
+++ config/elo_tweets.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+	'consumer'			=> 'YOUR CONSUMER KEY',
+	'consumer_secret'	=> 'YOUR CONSUMER SECRET',
+	'token'				=> 'YOUR TOKEN',
+	'token_secret'		=> 'YOUR TOKEN SECRET'
+	);
